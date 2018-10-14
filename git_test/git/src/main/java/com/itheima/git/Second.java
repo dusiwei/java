d70575bf5cd20c1fb14b7e6edabecbf828f8222b
Dusiwei <Dusiwei@126.com>
2018-10-14 19:03:36 +0800

    git测试第三次更新

--- git_test/git/src/main/java/com/itheima/git/Second.java
+++ git_test/git/src/main/java/com/itheima/git/Second.java
@@ -4,4 +4,8 @@ public class Second {
     public static void main(String[] args) {
         System.out.println("第二次更新");
     }
+
+    public void third() {
+        System.out.println("第三次更新");
+    }
 }
